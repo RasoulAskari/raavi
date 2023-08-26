e63471d866cd633dc1b3b58e5fd827b0009faea3
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-26 10:33:37 +0500

    romove role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,6 @@ class DatabaseSeeder extends Seeder
                 'name' => 'admin',
                 'email' => 'admin@admin.com',
                 'email_verified_at' => now(),
-                "role" => 'admin',
-                'permissions' => json_encode($permissions),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => '12',
             ]
