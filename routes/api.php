36300c41f2f54fe161d0cce9321826dbb148671b
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-26 17:50:57 +0500

    php artisan make:model Administrator -cm

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+// php artisan make:model Administrator -cm
+// php artisan make:model Ads -cm
+// php artisan make:model Attachment -cm
+// php artisan make:model Chat -cm
+// php artisan make:model Comment -cm
+// php artisan make:model CountriesStatesCities -cm
+// php artisan make:model ForgotPasswordSchema -cm
+// php artisan make:model MessageAttachment -cm
+// php artisan make:model MessageAttachmentSchema -cm
+// php artisan make:model Message -cm
+// php artisan make:model Notification -cm
+// php artisan make:model Post -cm
+// php artisan make:model Reason -cm
+// php artisan make:model Report -cm
+// php artisan make:model UserAccountPrefrence -cm
+// php artisan make:model User -cm
 
 /*
 |--------------------------------------------------------------------------
