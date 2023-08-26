720e8149cabf6c5a5567b10e93f64efebee00ed7
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-26 10:27:28 +0500

    fillable and hidden User model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create(
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                "role" => 'admin',
+                'permissions' => json_encode($permissions),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => '12',
+            ]
+        );
     }
 }
