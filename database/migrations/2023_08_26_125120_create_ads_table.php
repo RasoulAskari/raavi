37ac8ea6748512fe716ca81363df01b4361eecdd
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-26 19:33:57 +0500

    add migration schema

--- database/migrations/2023_08_26_125120_create_ads_table.php
+++ database/migrations/2023_08_26_125120_create_ads_table.php
@@ -13,6 +13,34 @@ return new class extends Migration
     {
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
+            $table->string("post_link");
+            $table->integer("audience_count");
+            $table->integer("views_count");
+            $table->timestamp("expire_at");
+            $table
+                ->enum("status", ["active", "disabled", "expired"])
+                ->index("ads_status_index", "hash")
+                ->nullable();
+            $table
+                ->integer("post_id")
+                ->unsigned()
+                ->index("ads_post_id_index", "hash")
+                ->nullable();
+            $table->foreign("post_id")->references("posts.id")->deferrable("deferred");
+            $table
+                ->uuid("created_by")
+                ->references("administrators.id")
+                ->deferrable("deferred")
+                ->unsigned()
+                ->nullable()
+                ->onDelete("SET NULL")
+                ->index("ads_created_by_index", "hash");
+
+            $table->string("expire_job_id");
+
+            $table->timestamps(true, false);
+            $table->timestamp("deleted_at");
+
             $table->timestamps();
         });
     }
