223b51ef9c846184d46f7df50c7ee22adc662e22
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-27 10:15:02 +0500

    message migration schema

--- database/migrations/2023_08_26_125325_create_message_attachments_table.php
+++ database/migrations/2023_08_26_125325_create_message_attachments_table.php
@@ -13,6 +13,50 @@ return new class extends Migration
     {
         Schema::create('message_attachments', function (Blueprint $table) {
             $table->id();
+            $table->increments("id")->index("message_attachment_id_index", "hash");
+            $table
+                ->string("url")
+                ->index("message_attachment_url_index", "btree")
+                ->notNullable();
+            $table
+                ->integer("height")
+                ->index("message_attachment_height_index", "hash")
+                ->nullable();
+            $table
+                ->integer("width")
+                ->index("message_attachment_width_index", "hash")
+                ->nullable();
+            $table->string("duration")->nullable();
+            $table->string("mime_type")->index("amessage_ttachment_mime_type_index", "hash");
+            $table->integer("size")->nullable();
+            $table->timestamps(true, true, false);
+            $table->enum("uploaded_by_type", ["admin", "user"]);
+            $table
+                ->uuid("uploaded_by")
+                ->index("message_attachment_uploaded_by_index", "hash");
+
+            $table->timestamps();
+        });
+        Schema::create('message_attachment_parent_schemas', function (Blueprint $table) {
+            $table
+                ->integer("parent_id")
+                ->unsigned()
+                ->index("message_attachment_parent_id_index", "hash")
+                ->nullable();
+            $table
+                ->foreign("parent_id")
+                ->references("message_attachments.id")
+                ->deferrable("deferred");
+            $table
+                ->integer("thumbnail_id")
+                ->unsigned()
+                ->index("message_attachment_thumbnail_id_index", "hash")
+                ->nullable();
+            $table
+                ->foreign("thumbnail_id")
+                ->references("message_attachments.id")
+                ->deferrable("deferred");
+
             $table->timestamps();
         });
     }
