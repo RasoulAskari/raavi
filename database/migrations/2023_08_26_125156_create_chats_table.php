1d7a0c0eadd8dfd964b806859cddb4f74963aadc
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-27 09:10:06 +0500

    chat migration schema

--- database/migrations/2023_08_26_125156_create_chats_table.php
+++ database/migrations/2023_08_26_125156_create_chats_table.php
@@ -13,7 +13,28 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->boolean("is_accepted")->defaultTo(true);
+            $table->timestamps(true, true);
+            $table->timestamp("last_messaged_at");
+            $table->json("recipients")->nullable();
+
             $table->timestamps();
+        });
+
+        Schema::create('userChatSchema', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->integer("chat_id")
+                ->unsigned()
+                ->notNullable()
+                ->index("user_chat_chat_id_index", "hash");
+            $table->foreign("chat_id")->references("chats.id")->deferrable("deferred");
+            $table
+                ->uuid("user_id")
+                ->references("users.id")
+                ->deferrable("deferred")
+                ->index("user_chat_user_id_index", "hash");
         });
     }
 
