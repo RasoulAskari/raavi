2a579de498ad3392da5940aa13d7e4cbfe0d5e82
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-28 10:10:27 +0500

    block user migration schema

---
+++ database/migrations/2023_08_28_043130_create_blocked_user_schemas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'blocked_user_schemas',
+            function (Blueprint $table) {
+                $table->id();
+                $table
+                    ->uuid("user_id")
+                    ->references("users.id")
+                    ->deferrable("deferred")
+                    ->index("user_block_user_id_index", "hash");
+                $table
+                    ->uuid("blocked_user_id")
+                    ->references("users.id")
+                    ->deferrable("deferred")
+                    ->index("user_block_blocked_id_index", "hash");
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blocked_user_schemas');
+    }
+};
