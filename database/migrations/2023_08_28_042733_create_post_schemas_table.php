7e615fe7a50a326b16db9b06ccf64e11e2eac064
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-28 09:57:46 +0500

    post migration schema

--- database/migrations/2023_08_28_042733_create_post_schemas_table.php
+++ database/migrations/2023_08_28_042733_create_post_schemas_table.php
@@ -11,8 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_schemas', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->text("content")->index("post_content_index", "btree")->nullable();
+            $table->enum("privacy", ["public", "followers", "only_me"])->nullable();
+            $table->enum("post_type", ["profile", "cover", "share", "post"])->nullable();
+            $table
+                ->enum("status", ["published", "suspended"])
+                ->index("post_status_index", "hash")
+                ->default("published");
+
+            $table->enum("attachment_type", ["none", "img", "video"])->nullable();
+            $table
+                ->uuid("created_by")
+                ->references("users->id")
+                ->deferrable("deferred")
+                ->nullable()
+                ->onDelete("SET NULL")
+                ->index()
+                ->index("post_created_by_index", "hash");
+
             $table->timestamps();
         });
     }
