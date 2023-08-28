c934c23a4d7bf58fa57cb13cdab96a1ab570808d
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-28 10:08:56 +0500

    user fcm token migration schema

---
+++ database/migrations/2023_08_28_043127_create_user_fcm_token_schemas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'user_fcm_token_schemas',
+            function (Blueprint $table) {
+                $table->id();
+                $table
+                    ->uuid("user_id")
+                    ->references("users.id")
+                    ->deferrable("deferred")
+                    ->index("user_fcm_token_user_id_index", "hash");
+                $table->text("fcm_token")->nullable();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_fcm_token_schemas');
+    }
+};
