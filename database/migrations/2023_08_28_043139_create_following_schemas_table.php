bf9d69f8fa6d1685c99a156f459528aebc2f85aa
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-28 10:14:12 +0500

    following migation schema

---
+++ database/migrations/2023_08_28_043139_create_following_schemas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'following_schemas',
+            function (Blueprint $table) {
+                $table->id();
+                $table
+                    ->uuid("followed_id")
+                    ->references("users.id")
+                    ->deferrable("deferred")
+                    ->index("following_followed_index", "hash");
+                $table
+                    ->uuid("follower_id")
+                    ->references("users.id")
+                    ->deferrable("deferred")
+                    ->index("following_follower_index", "hash");
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('following_schemas');
+    }
+};
