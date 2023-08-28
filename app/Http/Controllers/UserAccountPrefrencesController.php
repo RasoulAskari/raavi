126b9311187804cb291dd49e620c616b1e05ea54
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-28 09:37:00 +0500

    make models, migrations, controllers

---
+++ app/Http/Controllers/UserAccountPrefrencesController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAccountPrefrencesController extends Controller
+{
+    //
+}
---
+++ app/Models/PostHashSchema.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostHashSchema extends Model
+{
+    use HasFactory;
+}
