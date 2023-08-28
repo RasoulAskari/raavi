91ecf7c12a0d406e900c982a8d0d9b30cd176a5a
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-28 09:54:12 +0500

    message attachment parent schema

--- database/migrations/2023_08_28_042538_create_message_attachment_parent_schemas_table.php
+++ database/migrations/2023_08_28_042538_create_message_attachment_parent_schemas_table.php
@@ -12,7 +12,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('message_attachment_parent_schemas', function (Blueprint $table) {
-            $table->id();
+            $table
+                ->integer("parent_id")
+                ->unsigned()
+                ->index("message_attachment_parent_id_index", "hash")
+                ->nullable();
+            $table
+                ->foreign("parent_id")
+                ->references("message_attachments.id")
+                ->deferrable("deferred");
+            $table
+                ->integer("thumbnail_id")
+                ->unsigned()
+                ->index("message_attachment_thumbnail_id_index", "hash")
+                ->nullable();
+            $table
+                ->foreign("thumbnail_id")
+                ->references("message_attachments.id")
+                ->deferrable("deferred");
+
             $table->timestamps();
         });
     }
