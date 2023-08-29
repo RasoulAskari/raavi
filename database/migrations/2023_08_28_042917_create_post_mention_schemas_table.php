04e74d66ac2b8839ea71c75bc08a6b44df563d72
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-29 09:58:02 +0500

    post mention foreign key

--- database/migrations/2023_08_28_042917_create_post_mention_schemas_table.php
+++ database/migrations/2023_08_28_042917_create_post_mention_schemas_table.php
@@ -22,11 +22,11 @@ return new class extends Migration
                     ->nullable()
                     ->index("post_mention_post_id_index", "hash");
                 $table->foreign("post_id")->references("id")->on('post_schemas')->deferrable("deferred");
-                // $table
-                //     ->uuid("user_id")
-                //     ->references("id")->on('user_schemas')
-                //     ->deferrable("deferred")
-                //     ->index("post_mention_user_id_index", "hash");
+                $table
+                    ->uuid("user_id")
+                    ->references("id")->on('user_schemas')
+                    ->deferrable("deferred")
+                    ->index("post_mention_user_id_index", "hash");
                 $table->timestamps(true, true);
             }
         );
